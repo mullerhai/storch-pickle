6dc989b7eccece50afaee479bacc4811610b45dc
Muller Helen <hai710459649@gmail.com>
2025-07-12 23:29:39 +0800

    upgrade numpy version

--- src/test/scala/cifarPickleRead.scala
+++ src/test/scala/cifarPickleRead.scala
@@ -19,16 +19,17 @@ object cifarPickleRead {
     // dict_keys(['filenames', 'batch_label', 'fine_labels', 'coarse_labels', 'data'])
     val unpickler = new Unpickler
 //    val filePath = "D:\\data\\git\\storch-pickle\\random_structured_array.pkl"
-    val filePath = "D:\\data\\git\\storch-pickle\\random_array.pkl"
+//    val filePath = "D:\\data\\git\\storch-pickle\\random_array.pkl"
 //    val filePath = "D:\\data\\git\\storch-pickle\\newOnlyNumpyArray.pkl"
-//    val filePath = "D:\\data\\git\\storch-pickle\\newtests.pkl"
+    val filePath = "D:\\data\\git\\storch-pickle\\newtests.pkl"
 //    val filePath = "C:\\Users\\hai71\\PycharmProjects\\PythonProject1\\data\\cifar-100-python\\newtrain.pkl"
 //    val filePath = "D:\\data\\git\\storch-pickle\\newtestsnonumpy.pkl"
 //    val filePath = "D:\\data\\git\\storch-pickle\\tests.pkl"
     val res = unpickler.load(filePath)
     println("尝试读取 内容")
-    val npARR = res.asInstanceOf[MulitNumpyNdArray].numpyNdarray.toNDArray()
-    println(npARR.printArray())
+//    val npARR = res.asInstanceOf[MulitNumpyNdArray].numpyNdarray.toNDArray()
+//    val npARR = res.asInstanceOf[MulitNumpyNdArray].numpyNdarray.toNDArray()
+//    println(npARR.printArray())
 
 //    println(unpickler.memo)
 
@@ -38,7 +39,7 @@ object cifarPickleRead {
 //    println(resMap.get("batch_label").get)
 //    println(resMap.get("filenames").get)
 //    println(resMap.get("coarse_labels").get)
-    println(resMap.get("data").get)
+    println(resMap.get("data").get.asInstanceOf[MulitNumpyNdArray].numpyNdarray.toNDArray().printArray())
 
 //    println(resMap.get("fine_labels").get.asInstanceOf[mutable.ListBuffer[Int]].size)
     //    println(resMap.get("coarse_label_names").get)
